93794aff60e180415c219e33396b876857a75092
Vedant <dynamicvedant@yahoo.com>
2020-05-07 19:15:46 +0530

    7 videos

--- admin/add_customer.php
+++ admin/add_customer.php
@@ -1,134 +1,9 @@
 <?php 
-  session_start();
-  if($_SESSION['email']==true)
-  {
 
-  }
-  else
-  {
-    header('location:admin_login.php');
-  }
+  include('include/header.php');
+  include('include/sidebar.php');
+ ?>
 
-?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
-
-    <title>Add Customer</title>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-
-<!-- jQuery library -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-
-<!-- Popper JS -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-
-<!-- Latest compiled JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-
-
-    <!-- Custom styles for this template -->
-    <link href="css/admin_dashboard.css" rel="stylesheet">
-    <!--Data Table CSS-->
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-  </head>
-
-
-
-  <body>
-    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-      <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-      <ul class="navbar-nav px-3">
-        <li class="nav-item text-nowrap">
-          <a class="nav-link" href="logout.php">Sign out</a>
-        </li>
-      </ul>
-    </nav>
-
-    <div class="container-fluid">
-      <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-          <div class="sidebar-sticky">
-            <ul class="nav flex-column">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="home"></span>
-                  Dashboard <span class="sr-only">(current)</span>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file"></span>
-                  Orders
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="shopping-cart"></span>
-                  Products
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link active" href="customers.php">
-                  <span data-feather="users"></span>
-                  Customers
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="bar-chart-2"></span>
-                  Reports
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="layers"></span>
-                  Integrations
-                </a>
-              </li>
-            </ul>
-
-            <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-              <span>Saved reports</span>
-              <a class="d-flex align-items-center text-muted" href="#">
-                <span data-feather="plus-circle"></span>
-              </a>
-            </h6>
-            <ul class="nav flex-column mb-2">
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Current month
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Last quarter
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Social engagement
-                </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">
-                  <span data-feather="file-text"></span>
-                  Year-end sale
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
 
         <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
           <nav aria-label="breadcrumb">
@@ -147,14 +22,43 @@
 <!--               <a class="btn btn-primary" href="add_customer.php">Add Customer</a> -->
             </div>
           </div>
-          <div>
-            <form>
+          <div style="width: 50%;margin: 10px auto;">
+            <div class="alert alert-success" role="alert" style="visibility: hidden;">
+              Success Message
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+          </div>
+            <form name="customer_form" id="customer_form">
               <div class="form-group">
-                <label for="Customer Email"></label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="Enter Email" required>
+                <label for="Customer First Name">First Name</label>
+                <input type="text" class="form-control" name="firstname" id="firstname" placeholder="Enter First Name" required>
               </div><div class="form-group">
-                <label for="Customer Email"></label>
+                <label for="Customer Last Name">Last Name</label>
+                <input type="text" class="form-control" name="lastname" id="lastname" placeholder="Enter Last Name" required>
+              </div>
+              <div class="form-group">
+                <label for="Customer Email">Email</label>
                 <input type="email" class="form-control" name="email" id="email" placeholder="Enter Email" required>
+              </div>
+              <div class="form-group">
+                <label for="Customer Password">Set Password</label>
+                <input type="password" class="form-control" name="password" id="password" placeholder="Set Password" required>
+              </div>
+              <div class="form-group">
+                <label for="Customer Username">User Name</label>
+                <input type="text" class="form-control" name="username" id="username" placeholder="Enter UserName" required>
+              </div>
+              <div class="form-group">
+                <label for="Admin Type">Admin Type</label>
+                <select name="admin_type" id="admin_type" class="form-control">
+                  <option value="0">---</option>
+                  <option value="1">Super Admin</option>
+                  <option value="2">Customer Admin</option>
+                </select>
+              </div>
+              <div class="form-group">
+                <input type="submit" class="btn btn-block btn-success" name="submit" id="submit" value="Save">
               </div>
             </form>
           </div>
@@ -171,6 +75,7 @@
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
     <script src="../../assets/js/vendor/popper.min.js"></script>
     <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 
     <!-- Icons -->
     <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
@@ -189,5 +94,30 @@
 }
 );
     </script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+    $('#customer_form').submit(function(){
+      var email = $("#email").val();
+      var firstname = $("#firstname").val();
+      var lastname = $("#lastname").val();
+      var password = $("#password").val();
+      var admin_type = $("#admin_type").val();
+      var username = $("#username").val();
+      var data = $("#customer_form").serialize()
+
+      $.ajax({
+        type:"POST",
+        url:"Customer_add.php",
+        data:data,
+        success:function(data){
+            alert(data)      
+        }
+      });
+    });
+}
+);
+    </script>
   </body>
 </html>
+
+
